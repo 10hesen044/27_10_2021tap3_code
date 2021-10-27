111a904ca79af95fe4d7ebbe7c4f61b9c4a07ea5
10hesen044 <tu201906250@code.edu.az>
2021-10-27 12:07:53 +0400

    project added

---
+++ 27.10.2021tap3/27.10.2021tap3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _27._10._2021tap3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, result = 0;
+            Console.Write("Sayini daxil edin : ");
+            number = Convert.ToInt32(Console.ReadLine());
+            while (number > 0)
+            {
+                result += (number % 10);
+                number = number / 10;
+            }
+            Console.WriteLine("Sayinin Reqemlerin Cemi : " + result);
+            Console.ReadKey();
+        }
+    }
+}
